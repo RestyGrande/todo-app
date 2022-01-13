f2251980f06e18584dc41618f0fec53a473dcef2
Resty <grande.resty@gmail.com>
2022-01-13 10:21:27 +0800

    Add script on package file

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-function sayHello(name) {
-    return `hello, ${name}`;
+var container = document.getElementById("container");
+function countdown(init, final, interval) {
+    if (final === void 0) { final = 0; }
+    if (interval === void 0) { interval = 1; }
+    var current = init;
+    while (current > final) {
+        container.innerHTML = current;
+        current -= interval;
+    }
 }
-console.log(sayHello("Resty"));
+console.log(countdown(10));
