af16b6999b7820c1b6ae52632a493061e8ee866b
Resty <grande.resty@gmail.com>
2022-01-14 16:37:41 +0800

    Updated todo app

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-var container = document.getElementById("container");
-function countdown(init, final, interval) {
-    if (final === void 0) { final = 0; }
-    if (interval === void 0) { interval = 1; }
-    var current = init;
-    while (current > final) {
-        container.innerHTML = current;
-        current -= interval;
+var TodoService = /** @class */ (function () {
+    function TodoService(todos) {
+        this.todos = todos;
+        //
     }
-}
-console.log(countdown(10));
+    TodoService.prototype.add = function (todo) {
+        var newId = TodoService.getNextId();
+    };
+    TodoService.prototype.getAll = function () {
+        return this.todos;
+    };
+    TodoService.getNextId = function () {
+        return (TodoService.lastId += 1);
+    };
+    TodoService.lastId = 0;
+    return TodoService;
+}());
+var TodoState;
+(function (TodoState) {
+    TodoState[TodoState["New"] = 1] = "New";
+    TodoState[TodoState["Active"] = 2] = "Active";
+    TodoState[TodoState["Complete"] = 3] = "Complete";
+    TodoState[TodoState["Deleted"] = 4] = "Deleted";
+})(TodoState || (TodoState = {}));
+var todo = {
+    name: "Pick up drycleaning",
+    get state() {
+        return this._state;
+    },
+    set state(val) {
+        this._state = val;
+    }
+};
+todo.state = TodoState.Active;
